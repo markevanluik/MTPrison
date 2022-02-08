d1e0b41d2d5a871cdb8d0ef1c187d5433c09defa
Your Name <you@example.com>
2022-02-08 12:24:56 +0200

    PrisonerData testclass

---
+++ Tests/Data/Party/PrisonerDataTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MTPrison.Data.Party;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MTPrison.Tests.Data.Party
+{
+    [TestClass] public class PrisonerDataTests : BaseTests<PrisonerData>
+    {
+        [TestMethod] public void IdTest() => isProperty<string>();
+        [TestMethod] public void FirstNameTest() => isProperty<string?>();
+        [TestMethod] public void LastNameTest() => isProperty<string?>();
+        [TestMethod] public void OffenseTest() => isProperty<string?>();
+        [TestMethod] public void DoBTest() => isProperty<DateTime?>();
+        [TestMethod] public void DateOfReleaseTest() => isProperty<DateTime?>();
+        [TestMethod] public void DateOfImprisonmentTest() => isProperty<DateTime?>();
+    }
+}
