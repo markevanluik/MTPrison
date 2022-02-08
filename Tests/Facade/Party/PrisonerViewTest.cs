27862ec05bce42659deb88454201c0034efd33c6
Your Name <you@example.com>
2022-02-08 12:25:09 +0200

    PrisonerView testclass

---
+++ Tests/Facade/Party/PrisonerViewTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MTPrison.Facade.Party;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTPrison.Tests.Facade.Party
+{
+    [TestClass]
+    public class PrisonerViewTest : BaseTests<PrisonerView>
+    {
+        [TestMethod] public void IdTest() => isProperty<string>();
+        [TestMethod] public void FirstNameTest() => isProperty<string?>();
+        [TestMethod] public void LastNameTest() => isProperty<string?>();
+        [TestMethod] public void OffenseTest() => isProperty<string?>();
+        [TestMethod] public void DoBTest() => isProperty<DateTime?>();
+        [TestMethod] public void DateOfReleaseTest() => isProperty<DateTime?>();
+        [TestMethod] public void DateOfImprisonmentTest() => isProperty<DateTime?>();
+
+    }
+
+}
