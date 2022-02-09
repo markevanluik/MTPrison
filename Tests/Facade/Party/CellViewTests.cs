7088baafdfa6829ff23bbedd5aed8800454cee05
Tarmo Ploom <taploo@ttu.ee>
2022-02-10 00:35:32 +0200

    added Cells Tests

---
+++ Tests/Facade/Party/CellViewTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MTPrison.Facade.Party;
+
+namespace MTPrison.Tests.Facade.Party
+{
+    [TestClass] public class CellViewTest : BaseTests<CellView>
+    {
+        [TestMethod] public void IdTest() => isProperty<string>();
+        [TestMethod] public void CellNumberTest() => isProperty<int>();
+        [TestMethod] public void CapacityTest() => isProperty<int>();
+        [TestMethod] public void TypeTest() => isProperty<string?>();
+        [TestMethod] public void SectionTest() => isProperty<string?>();
+    }
+}
