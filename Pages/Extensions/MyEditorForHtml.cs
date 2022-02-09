7a8c3084b4f2d3b199fd124a4812a163e152de91
Your Name <you@example.com>
2022-02-09 13:43:53 +0200

    html generators

---
+++ Pages/Extensions/MyEditorForHtml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTPrison.Pages.Extensions
+{
+    public static class MyEditorForHtml
+    {
+        public static IHtmlContent MyEditorFor<TModel, TResult>(
+            this IHtmlHelper<TModel> h, Expression<Func<TModel, TResult>> e)
+        {
+            var s = htmlStrings(h, e);
+            return new HtmlContentBuilder(s);
+        }
+
+        private static List<object> htmlStrings<TModel, TResult>(IHtmlHelper<TModel> h, Expression<Func<TModel, TResult>> e)
+        {
+            var l = new List<object>();
+            l.Add(new HtmlString("<div class=\"row\">"));
+                l.Add(new HtmlString("<dd class=\"col-sm-2\">"));
+                    l.Add(h.LabelFor(e, null, new { @class = "control-label" }));
+                l.Add(new HtmlString("</dd>"));
+                l.Add(new HtmlString("<dd class=\"col-sm-10\">"));
+                    l.Add(h.EditorFor(e, new { htmlAttributes = new { @class = "form-control" } }));
+                    l.Add(h.ValidationMessageFor(e, null, new { @class = "text-danger"}));
+                l.Add(new HtmlString("</dd>"));
+            l.Add(new HtmlString("</div>"));
+            return l;
+        }
+    }
+}
---
+++ Pages/Extensions/MyViewerForHtml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTPrison.Pages.Extensions
+{
+    public static class MyViewerForHtml
+    {
+        public static IHtmlContent MyViewerFor<TModel, TResult>(
+            this IHtmlHelper<TModel> h, Expression<Func<TModel, TResult>> e)
+        {
+            var s = htmlStrings(h, e);
+            return new HtmlContentBuilder(s);
+        }
+
+        private static List<object> htmlStrings<TModel, TResult>(IHtmlHelper<TModel> h, Expression<Func<TModel, TResult>> e)
+        {
+            var l = new List<object>();
+            l.Add(new HtmlString("<dt class=\"col-sm-2\">"));
+            l.Add(h.LabelFor(e, null, new { @class = "control-label" }));
+            l.Add(new HtmlString("</dt>"));
+            l.Add(new HtmlString("<dd class=\"col-sm-10\">"));
+            l.Add(h.DisplayFor(e));
+            l.Add(new HtmlString("</dd>"));
+            return l;
+        }
+    }
+}
