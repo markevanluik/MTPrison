3d23f2ab3971f4e8838eaa01ce829c27923d6bfb
markevanluik <markevanluik@gmail.com>
2022-02-22 18:05:37 +0200

    modified template views and shared pages for Prisoner and Cell

--- Pages/Extensions/MyViewerForHtml.cs
+++ Pages/Extensions/MyViewerForHtml.cs
@@ -21,12 +21,14 @@ namespace MTPrison.Pages.Extensions
         private static List<object> htmlStrings<TModel, TResult>(IHtmlHelper<TModel> h, Expression<Func<TModel, TResult>> e)
         {
             var l = new List<object>();
+            l.Add(new HtmlString("<dl class=\"row\">"));
             l.Add(new HtmlString("<dt class=\"col-sm-2\">"));
-            l.Add(h.LabelFor(e, null, new { @class = "control-label" }));
+                l.Add(h.DisplayNameFor(e));
             l.Add(new HtmlString("</dt>"));
             l.Add(new HtmlString("<dd class=\"col-sm-10\">"));
-            l.Add(h.DisplayFor(e));
+                l.Add(h.DisplayFor(e));
             l.Add(new HtmlString("</dd>"));
+            l.Add(new HtmlString("</dl>"));
             return l;
         }
     }
