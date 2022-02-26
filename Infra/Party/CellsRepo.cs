2f12f41b8e1ab2076fb75d2fe24c97b1142eef19
taploo22 <tarmoploom@outlook.com>
2022-02-26 17:20:51 +0200

    added CellsRepo

---
+++ Infra/Party/CellsRepo.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MTPrison.Data.Party;
+using MTPrison.Domain.Party;
+
+namespace MTPrison.Infra.Party {
+    public class CellsRepo : Repo<Cell, CellData>, ICellsRepo {
+        public CellsRepo(DbContext c, DbSet<CellData> s) : base(c, s) { }
+        protected override Cell toDomain(CellData d) => new Cell(d);
+    }
+}
