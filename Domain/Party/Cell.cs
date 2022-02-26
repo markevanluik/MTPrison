cac85252df9db9b865b9d89765138b6b461c8e00
taploo22 <tarmoploom@outlook.com>
2022-02-26 17:19:59 +0200

    changed  data > from > Data

--- Domain/Party/Cell.cs
+++ Domain/Party/Cell.cs
@@ -1,18 +1,16 @@
 ﻿using MTPrison.Data.Party;
 
 namespace MTPrison.Domain.Party {
-    public class Cell {
+    public class Cell : Entity<CellData>{
         private const string defaultStr = "Undefined";
         private const int defaultInt = int.MinValue;
-        private CellData data;
         public Cell() : this(new CellData()) { }
-        public Cell(CellData d) => data = d;
-        public string Id => data?.Id ?? defaultStr;
-        public int CellNumber => data?.CellNumber ?? defaultInt;
-        public int Capacity => data?.Capacity ?? defaultInt;
-        public string? Type => data?.Type ?? defaultStr;
-        public string? Section => data?.Section ?? defaultStr;
-        public CellData Data => data;
+        public Cell(CellData d) : base(d) { }
+        public string Id => Data?.Id ?? defaultStr;
+        public int CellNumber => Data?.CellNumber ?? defaultInt;
+        public int Capacity => Data?.Capacity ?? defaultInt;
+        public string? Type => Data?.Type ?? defaultStr;
+        public string? Section => Data?.Section ?? defaultStr;
 
         //public List<Prisoner> Occupants { get; set; }
     }
