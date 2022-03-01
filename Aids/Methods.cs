26b23ab10f2efc24c122c0d645c4fbdeda0cee29
markevanluik <markevanluik@gmail.com>
2022-03-01 23:53:45 +0200

    added bunch of utility methods

---
+++ Aids/Methods.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+namespace MTPrison.Aids {
+    public static class Methods {
+        public static bool HasAttribute<TAttribute>(this MethodInfo? m) where TAttribute : Attribute
+            => Safe.Run(() => m?.GetCustomAttributes<TAttribute>()?.FirstOrDefault() is not null, false);
+    }
+}
