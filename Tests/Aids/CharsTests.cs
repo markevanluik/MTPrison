f300e9b5f97bec5097cc585572f02d6e60219f20
markevanluik <markevanluik@gmail.com>
2022-03-02 23:01:53 +0200

    Tests for Aids.Chars

--- Tests/Aids/CharsTests.cs
+++ Tests/Aids/CharsTests.cs
@@ -1,5 +1,14 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MTPrison.Aids;
 
 namespace MTPrison.Tests.Aids {
-    [TestClass] public class CharsTests : IsTypeTested { }
+    [TestClass] public class CharsTests : IsTypeTested {
+        [TestMethod] public void IsNameCharTest() {
+            Assert.IsTrue(Chars.IsNameChar('A'));
+            Assert.IsTrue(Chars.IsNameChar('9'));
+            Assert.IsTrue(Chars.IsNameChar('.'));
+            Assert.IsTrue(Chars.IsNameChar('_'));
+            Assert.IsFalse(Chars.IsNameChar(':'));
+        }
+    }
 }
