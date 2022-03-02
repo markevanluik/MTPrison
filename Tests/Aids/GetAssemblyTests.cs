06fed8aa463cf1b152735f81a7081d80c4f344ed
markevanluik <markevanluik@gmail.com>
2022-03-02 23:02:42 +0200

    Test methods structure for Aids.GetAssembly

--- Tests/Aids/GetAssemblyTests.cs
+++ Tests/Aids/GetAssemblyTests.cs
@@ -1,5 +1,10 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace MTPrison.Tests.Aids {
-    [TestClass] public class GetAssemblyTests : IsTypeTested { }
+    [TestClass] public class GetAssemblyTests : IsTypeTested {
+        [TestMethod] public void ByNameTest() => isInconclusive();
+        [TestMethod] public void OfTypeTest() => isInconclusive();
+        [TestMethod] public void TypesTest() => isInconclusive();
+        [TestMethod] public void TypeTest() => isInconclusive();
+    }
 }
