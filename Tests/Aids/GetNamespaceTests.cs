58f7623aa4a1df73815eb99b10002bcec1417d86
taploo22 <tarmoploom@outlook.com>
2022-03-04 20:01:14 +0200

    added GetNamespaceTests

--- Tests/Aids/GetNamespaceTests.cs
+++ Tests/Aids/GetNamespaceTests.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MTPrison.Aids;
 
 namespace MTPrison.Tests.Aids {
     [TestClass] public class GetNamespaceTests : IsTypeTested {
-        [TestMethod] public void OfTypeTest() => isInconclusive();
+        [TestMethod] public void OfTypeTest() {
+            Assert.AreEqual(GetNamespace.OfType(this), "MTPrison.Tests.Aids");  // this can be..Tests.. bc OfTypeTest() is very basic
+            Assert.AreNotEqual(GetNamespace.OfType(this), "....");
+        }
     }
 }
