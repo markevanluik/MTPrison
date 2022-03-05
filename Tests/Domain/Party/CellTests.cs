c8e22df63927f3a87679664d19642b64c02c9ebc
taploo22 <tarmoploom@outlook.com>
2022-03-05 18:37:28 +0200

    added CellTests > get simple version

--- Tests/Domain/Party/CellTests.cs
+++ Tests/Domain/Party/CellTests.cs
@@ -3,11 +3,21 @@ using MTPrison.Domain.Party;
 
 namespace MTPrison.Tests.Domain.Party {
     [TestClass] public class CellTests : BaseTests<Cell> {
-        [TestMethod] public void IdTest() => isInconclusive();
-        [TestMethod] public void CellNumberTest() => isInconclusive();
-        [TestMethod] public void CapacityTest() => isInconclusive();
-        [TestMethod] public void TypeTest() => isInconclusive();
-        [TestMethod] public void SectionTest() => isInconclusive();
-
+        private readonly Cell cell = new() { };
+        [TestMethod] public void IdTest() {
+            Assert.AreEqual("Undefined", cell.Id);
+        }
+        [TestMethod] public void CellNumberTest() {
+            Assert.AreEqual(0, cell.CellNumber);
+        }
+        [TestMethod] public void CapacityTest() {
+            Assert.AreEqual(0, cell.Capacity);
+        }
+        [TestMethod] public void TypeTest() {
+            Assert.AreEqual("Undefined", cell.Type);
+        }
+        [TestMethod] public void SectionTest() {
+            Assert.AreEqual("Undefined", cell.Section);
+        }
     }
 }
