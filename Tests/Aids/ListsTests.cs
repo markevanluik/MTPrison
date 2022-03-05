4c9a9dbd9d43de03f26a6dd98296aab5af70d8c6
taploo22 <tarmoploom@outlook.com>
2022-03-05 15:06:08 +0200

    updated ListsTests, better/safer Testing

--- Tests/Aids/ListsTests.cs
+++ Tests/Aids/ListsTests.cs
@@ -13,10 +13,10 @@ namespace MTPrison.Tests.Aids {
         }
         [TestMethod] public void RemoveTest() {
             var empty = new List<dynamic>();
-            Assert.AreEqual(0, empty.Remove(x => x == "1"));  // trying remove "1", 0 removes
+            Assert.AreEqual(0, empty.Remove(x => x.Equals("1")));  // trying remove "1", 0 removes
 
             var list = new List<dynamic> { 1, 1, 3};
-            Assert.AreEqual(2, list.Remove(x => x == 1));     // trying remove 2  1's
+            Assert.AreEqual(2, list.Remove(x => x.Equals(1)));     // trying remove 2  1's
             Assert.AreEqual(3, list.GetFirst());              // 3 remains
         }
         [TestMethod] public void IsEmptyTest() {
@@ -28,10 +28,10 @@ namespace MTPrison.Tests.Aids {
         }
         [TestMethod] public void ContainsItemTest() {
             var list = new List<dynamic>();
-            Assert.IsFalse(list.ContainsItem(x => x == 1));
+            Assert.IsFalse(list.ContainsItem(x => x.Equals(1)));
 
-            list.Add(2); list.Add(3); list.Add(5);
-            Assert.IsTrue(list.ContainsItem(x => x == 5));
+            list.Add(2); list.Add(3); list.Add("5");
+            Assert.IsTrue(list.ContainsItem(x => x.Equals("5")));   // Equals() is safer with lambda
         }
     }
 }
