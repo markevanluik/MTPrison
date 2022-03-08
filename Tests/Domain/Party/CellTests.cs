1a0c5424926dc0ac9eef5ec4efc17a7e1b78319e
taploo22 <tarmoploom@outlook.com>
2022-03-08 16:37:29 +0200

    new CellTests > isProperty ver

--- Tests/Domain/Party/CellTests.cs
+++ Tests/Domain/Party/CellTests.cs
@@ -3,21 +3,10 @@ using MTPrison.Domain.Party;
 
 namespace MTPrison.Tests.Domain.Party {
     [TestClass] public class CellTests : BaseTests<Cell> {
-        private readonly Cell cell = new();
-        [TestMethod] public void IdTest() {
-            areEqual("Undefined", cell.Id);
-        }
-        [TestMethod] public void CellNumberTest() {
-            areEqual(0, cell.CellNumber);
-        }
-        [TestMethod] public void CapacityTest() {
-            areEqual(0, cell.Capacity);
-        }
-        [TestMethod] public void TypeTest() {
-            areEqual("Undefined", cell.Type);
-        }
-        [TestMethod] public void SectionTest() {
-            areEqual("Undefined", cell.Section);
-        }
+        [TestMethod] public void IdTest() => isProperty<string?>();
+        [TestMethod] public void CellNumberTest() => isProperty<int>();
+        [TestMethod] public void CapacityTest() => isProperty<int>();
+        [TestMethod] public void TypeTest() => isProperty<string?>();
+        [TestMethod] public void SectionTest() => isProperty<string?>();
     }
 }
