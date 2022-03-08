e7502b6e5d98baf13af9c52e10b5369295895f87
taploo22 <tarmoploom@outlook.com>
2022-03-08 16:37:50 +0200

    alternative BaseTests version

--- Tests/BaseTests.cs
+++ Tests/BaseTests.cs
@@ -10,16 +10,18 @@ namespace MTPrison.Tests {
             var memberName = getCallingMember(nameof(isProperty)).Replace("Test", string.Empty);
             var propertyInfo = obj.GetType().GetProperty(memberName);
             isNotNull(propertyInfo);
-            if (isNullOrDefault(value)) value = random<T>();
-            if (canWrite(propertyInfo, isReadOnly)) propertyInfo.SetValue(obj, value);
-            areEqual(value, propertyInfo.GetValue(obj));
+            var canWrite = propertyInfo?.CanWrite ?? false;
+            if (isNullOrDefault(value) && canWrite) value = random<T>();
+            if (canWrite) propertyInfo?.SetValue(obj, value);
+            else value = (T?)propertyInfo?.GetValue(obj);
+            areEqual(value, propertyInfo?.GetValue(obj));
         }
         private static bool isNullOrDefault<T>(T? value) => value?.Equals(default(T)) ?? true;
-        private static bool canWrite(PropertyInfo i, bool isReadOnly) {
-            var canWrite = i?.CanWrite ?? false;
-            areEqual(canWrite, !isReadOnly);
-            return canWrite;
-        }
+        //private static bool canWrite(PropertyInfo i, bool isReadOnly) {
+        //    var canWrite = i?.CanWrite ?? false;
+        //    areEqual(canWrite, !isReadOnly);
+        //    return canWrite;
+        //}
         private static T random<T>() => GetRandom.Value<T>();
         private string getCallingMember(string memberName) {
             var s = new StackTrace();
@@ -32,5 +34,30 @@ namespace MTPrison.Tests {
             }
             return string.Empty;
         }
+        //protected void isProperty<T>(T? value = default, bool isReadOnly = false) {
+        //    var memberName = getCallingMember(nameof(isProperty)).Replace("Test", string.Empty);
+        //    var propertyInfo = obj.GetType().GetProperty(memberName);
+        //    isNotNull(propertyInfo);
+        //    if (isNullOrDefault(value)) value = random<T>();
+        //    if (canWrite(propertyInfo, isReadOnly)) propertyInfo.SetValue(obj, value);
+        //    areEqual(value, propertyInfo.GetValue(obj));
+        //}
+        //private static bool isNullOrDefault<T>(T? value) => value?.Equals(default(T)) ?? true;
+        //private static bool canWrite(PropertyInfo i, bool isReadOnly) {
+        //    var canWrite = i?.CanWrite ?? false;
+        //    areEqual(canWrite, !isReadOnly);
+        //    return canWrite;
+        //}
+        //private static T random<T>() => GetRandom.Value<T>();
+        //private string getCallingMember(string memberName) {
+        //    var s = new StackTrace();
+        //    var isNext = false;
+        //    for (var i = 0; s.FrameCount - 1 > 0; i++) {
+        //        var n = s.GetFrame(i)?.GetMethod()?.Name ?? string.Empty;
+        //        if (n is "MoveNext" or "Start") continue;
+        //        if (isNext) return n;
+        //        if (n == memberName) isNext = true;
+        //    }
+        //    return string.Empty;
     }
 }
