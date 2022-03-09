8b1b5c3807439eeb679335b956e7bbd698823bc9
markevanluik <markevanluik@gmail.com>
2022-03-09 11:40:03 +0200

    refactored ItemId to a more abstract form

--- MTPrisonApp/Pages/Cells/CellsPage.cs
+++ MTPrisonApp/Pages/Cells/CellsPage.cs
@@ -8,8 +8,9 @@ using MTPrisonApp.Data;
 namespace MTPrisonApp.Pages.Cells {
     public class CellsPage : PageModel {
         private readonly ICellsRepo repo;
-        [BindProperty] public CellView Cell { get; set; }
-        public IList<CellView> Cells { get; set; }
+        [BindProperty] public CellView Item { get; set; }
+        public IList<CellView> Items { get; set; }
+        public string ItemId => Item?.Id ?? string.Empty;
         //public SelectList Sections { get; set; }
         //[BindProperty(SupportsGet = true)] public string? CellSection { get; set; }
         //public SelectList Types { get; set; }
@@ -18,33 +19,33 @@ namespace MTPrisonApp.Pages.Cells {
         public IActionResult OnGetCreate() => Page();
         public async Task<IActionResult> OnPostCreateAsync() {
             if (!ModelState.IsValid) return Page();
-            await repo.AddAsync(new CellViewFactory().Create(Cell));
+            await repo.AddAsync(new CellViewFactory().Create(Item));
             return RedirectToPage("./Index", "Index");
         }
         public async Task<IActionResult> OnGetIndexAsync() {
             var list = await repo.GetAsync();
-            Cells = new List<CellView>();
-            list.ForEach(x => Cells.Add(new CellViewFactory().Create(x)));
+            Items = new List<CellView>();
+            list.ForEach(x => Items.Add(new CellViewFactory().Create(x)));
             return Page();
         }
         public async Task<IActionResult> OnGetDetailsAsync(string id) {
-            Cell = await GetCell(id);
-            return Cell == null ? NotFound() : Page();
+            Item = await GetCell(id);
+            return Item == null ? NotFound() : Page();
         }
         public async Task<IActionResult> OnGetEditAsync(string id) {
-            Cell = await GetCell(id);
-            return Cell == null ? NotFound() : Page();
+            Item = await GetCell(id);
+            return Item == null ? NotFound() : Page();
         }
         public async Task<IActionResult> OnPostEditAsync() {
             if (!ModelState.IsValid) return Page();
-            var obj = new CellViewFactory().Create(Cell);
+            var obj = new CellViewFactory().Create(Item);
             var updated = await repo.UpdateAsync(obj);
             if (!updated) return NotFound();
             return RedirectToPage("./Index", "Index");
         }
         public async Task<IActionResult> OnGetDeleteAsync(string id) {
-            Cell = await GetCell(id);
-            return Cell == null ? NotFound() : Page();
+            Item = await GetCell(id);
+            return Item == null ? NotFound() : Page();
         }
         public async Task<IActionResult> OnPostDeleteAsync(string id) {
             if (id == null) return NotFound();
--- MTPrisonApp/Pages/Prisoners/PrisonersPage.cs
+++ MTPrisonApp/Pages/Prisoners/PrisonersPage.cs
@@ -8,8 +8,9 @@ using MTPrisonApp.Data;
 namespace MTPrisonApp.Pages.Prisoners {
     public class PrisonersPage : PageModel {
         private readonly IPrisonersRepo repo;
-        [BindProperty] public PrisonerView Prisoner { get; set; }
-        public IList<PrisonerView> Prisoners { get; set; }
+        [BindProperty] public PrisonerView Item { get; set; }
+        public IList<PrisonerView> Items { get; set; }
+        public string ItemId => Item?.Id?? string.Empty;
         //[BindProperty(SupportsGet = true)] public string? SearchString { get; set; }
         //public SelectList Offenses { get; set; }
         //[BindProperty(SupportsGet = true)] public string? PrisonerOffense { get; set; }
@@ -17,33 +18,33 @@ namespace MTPrisonApp.Pages.Prisoners {
         public IActionResult OnGetCreate() => Page();
         public async Task<IActionResult> OnPostCreateAsync() {
             if (!ModelState.IsValid) return Page();
-            await repo.AddAsync(new PrisonerViewFactory().Create(Prisoner));
+            await repo.AddAsync(new PrisonerViewFactory().Create(Item));
             return RedirectToPage("./Index", "Index");
         }
         public async Task<IActionResult> OnGetIndexAsync() {
             var list = await repo.GetAsync();
-            Prisoners = new List<PrisonerView>();
-            list.ForEach(x => Prisoners.Add(new PrisonerViewFactory().Create(x)));
+            Items = new List<PrisonerView>();
+            list.ForEach(x => Items.Add(new PrisonerViewFactory().Create(x)));
             return Page();
         }
         public async Task<IActionResult> OnGetDetailsAsync(string id) {
-            Prisoner = await GetPrisoner(id);
-            return Prisoner == null ? NotFound() : Page();
+            Item = await GetPrisoner(id);
+            return Item == null ? NotFound() : Page();
         }
         public async Task<IActionResult> OnGetEditAsync(string id) {
-            Prisoner = await GetPrisoner(id);
-            return Prisoner == null ? NotFound() : Page();
+            Item = await GetPrisoner(id);
+            return Item == null ? NotFound() : Page();
         }
         public async Task<IActionResult> OnPostEditAsync() {
             if (!ModelState.IsValid) return Page();
-            var obj = new PrisonerViewFactory().Create(Prisoner);
+            var obj = new PrisonerViewFactory().Create(Item);
             var updated = await repo.UpdateAsync(obj);
             if (!updated) return NotFound();
             return RedirectToPage("./Index", "Index");
         }
         public async Task<IActionResult> OnGetDeleteAsync(string id) {
-            Prisoner = await GetPrisoner(id);
-            return Prisoner == null ? NotFound() : Page();
+            Item = await GetPrisoner(id);
+            return Item == null ? NotFound() : Page();
         }
         public async Task<IActionResult> OnPostDeleteAsync(string id) {
             if (id == null) return NotFound();
