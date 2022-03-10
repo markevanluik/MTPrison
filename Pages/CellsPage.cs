f93280b181475b9dd21e403969c7cd1b51846a33
taploo22 <tarmoploom@outlook.com>
2022-03-10 22:17:38 +0200

    CellsPage : BasePage

--- Pages/CellsPage.cs
+++ Pages/CellsPage.cs
@@ -1,57 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using MTPrison.Domain.Party;
+﻿using MTPrison.Domain.Party;
 using MTPrison.Facade.Party;
-using MTPrison.Infra;
-using MTPrison.Infra.Party;
 
 namespace MTPrison.Pages {
-    public class CellsPage : PageModel {
-        private readonly ICellsRepo repo;
-        [BindProperty] public CellView Item { get; set; }
-        public IList<CellView> Items { get; set; }
-        public string ItemId => Item?.Id ?? string.Empty;
-        //public SelectList Sections { get; set; }
-        //[BindProperty(SupportsGet = true)] public string? CellSection { get; set; }
-        //public SelectList Types { get; set; }
-        //[BindProperty(SupportsGet = true)] public string? CellType { get; set; }
-        public CellsPage(PrisonDb c) => repo = new CellsRepo(c, c.Cells);
-        public IActionResult OnGetCreate() => Page();
-        public async Task<IActionResult> OnPostCreateAsync() {
-            if (!ModelState.IsValid) return Page();
-            await repo.AddAsync(new CellViewFactory().Create(Item));
-            return RedirectToPage("./Index", "Index");
-        }
-        public async Task<IActionResult> OnGetIndexAsync() {
-            var list = await repo.GetAsync();
-            Items = new List<CellView>();
-            list.ForEach(x => Items.Add(new CellViewFactory().Create(x)));
-            return Page();
-        }
-        public async Task<IActionResult> OnGetDetailsAsync(string id) {
-            Item = await GetCell(id);
-            return Item == null ? NotFound() : Page();
-        }
-        public async Task<IActionResult> OnGetEditAsync(string id) {
-            Item = await GetCell(id);
-            return Item == null ? NotFound() : Page();
-        }
-        public async Task<IActionResult> OnPostEditAsync() {
-            if (!ModelState.IsValid) return Page();
-            var obj = new CellViewFactory().Create(Item);
-            var updated = await repo.UpdateAsync(obj);
-            if (!updated) return NotFound();
-            return RedirectToPage("./Index", "Index");
-        }
-        public async Task<IActionResult> OnGetDeleteAsync(string id) {
-            Item = await GetCell(id);
-            return Item == null ? NotFound() : Page();
-        }
-        public async Task<IActionResult> OnPostDeleteAsync(string id) {
-            if (id == null) return NotFound();
-            await repo.DeleteAsync(id);
-            return RedirectToPage("./Index", "Index");
-        }
-        private async Task<CellView> GetCell(string id) => new CellViewFactory().Create(await repo.GetAsync(id));
+    public class CellsPage : BasePage<CellView, Cell, ICellsRepo> {
+        public CellsPage(ICellsRepo r) : base(r) { }
+        protected override Cell toObject(CellView item) => new CellViewFactory().Create(item);
+        protected override CellView toView(Cell entity) => new CellViewFactory().Create(entity);
     }
 }
