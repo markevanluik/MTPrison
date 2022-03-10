68771e6a95843501dcd5f4322484ada02c6992ff
taploo22 <tarmoploom@outlook.com>
2022-03-10 22:16:23 +0200

    changed CellsRepo context >PrisonDB

--- Infra/Party/CellsRepo.cs
+++ Infra/Party/CellsRepo.cs
@@ -1,10 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
-using MTPrison.Data.Party;
+﻿using MTPrison.Data.Party;
 using MTPrison.Domain.Party;
 
 namespace MTPrison.Infra.Party {
     public class CellsRepo : Repo<Cell, CellData>, ICellsRepo {
-        public CellsRepo(DbContext c, DbSet<CellData> s) : base(c, s) { }
+        public CellsRepo(PrisonDb db) : base(db, db.Cells) { }
         protected override Cell toDomain(CellData d) => new Cell(d);
     }
 }
