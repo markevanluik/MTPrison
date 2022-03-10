8d49faad48b869cfc88a0b37e44ca1dd3ff1a950
taploo22 <tarmoploom@outlook.com>
2022-03-10 22:19:38 +0200

    cleanup CellsPageTest

--- Tests/Pages/CellsPageTests.cs
+++ Tests/Pages/CellsPageTests.cs
@@ -1,17 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace MTPrison.Tests.Pages {
-    [TestClass] public class CellsPageTests : IsTypeTested {
-        [TestMethod] public void ItemTest() => isInconclusive();
-        [TestMethod] public void ItemsTest() => isInconclusive();
-        [TestMethod] public void ItemIdTest() => isInconclusive();
-        [TestMethod] public void OnGetCreateTest() => isInconclusive();
-        [TestMethod] public void OnPostCreateAsyncTest() => isInconclusive();
-        [TestMethod] public void OnGetIndexAsyncTest() => isInconclusive();
-        [TestMethod] public void OnGetDetailsAsyncTest() => isInconclusive();
-        [TestMethod] public void OnGetEditAsyncTest() => isInconclusive();
-        [TestMethod] public void OnPostEditAsyncTest() => isInconclusive();
-        [TestMethod] public void OnGetDeleteAsyncTest() => isInconclusive();
-        [TestMethod] public void OnPostDeleteAsyncTest() => isInconclusive();
-    }
+    [TestClass] public class CellsPageTests : IsTypeTested { }
+
 }
