4556efe49c8bef446dd6a6666cb52f21ceb7cf2d
markevanluik <markevanluik@gmail.com>
2022-03-16 18:40:19 +0200

    Adds more methods to TestAsserts

--- Tests/TestAsserts.cs
+++ Tests/TestAsserts.cs
@@ -4,9 +4,12 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace MTPrison.Tests {
     public abstract class TestAsserts {
-        protected void isInconclusive(string? s = null) => Assert.Inconclusive(s ?? string.Empty);
-        protected static void isNotNull([NotNull] object? o = null) => Assert.IsNotNull(o);
-        protected static void areEqual(object? expected, object? actual) => Assert.AreEqual(expected, actual);
-        protected static void isInstanceOfType(object o, Type expectedType) => Assert.IsInstanceOfType(o, expectedType);
+        protected static void isTrue(bool? b, string? msg = null) => Assert.IsTrue(b ?? false, msg ?? string.Empty);
+        protected static void isFalse(bool? b, string? msg = null) => Assert.IsFalse(b ?? true, msg ?? string.Empty);
+        protected static void isInconclusive(string? s = null) => Assert.Inconclusive(s ?? string.Empty);
+        protected static void isNotNull([NotNull] object? o = null, string? msg = null) => Assert.IsNotNull(o, msg ?? string.Empty);
+        protected static void areEqual(object? expected, object? actual, string? msg = null) => Assert.AreEqual(expected, actual, msg ?? string.Empty);
+        protected static void areNotEqual(object? expected, object? actual, string? msg = null) => Assert.AreNotEqual(expected, actual, msg ?? string.Empty);
+        protected static void isInstanceOfType(object o, Type expectedType, string? msg = null) => Assert.IsInstanceOfType(o, expectedType, msg ?? string.Empty);
     }
 }
