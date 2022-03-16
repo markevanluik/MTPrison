40cdb459160dc178dea4d2a7adef778f2e7327a7
markevanluik <markevanluik@gmail.com>
2022-03-16 19:28:42 +0200

    Add unit test

--- Tests/Aids/CharsTests.cs
+++ Tests/Aids/CharsTests.cs
@@ -4,11 +4,18 @@ using MTPrison.Aids;
 namespace MTPrison.Tests.Aids {
     [TestClass] public class CharsTests : IsTypeTested {
         [TestMethod] public void IsNameCharTest() {
-            Assert.IsTrue(Chars.IsNameChar('A'));
-            Assert.IsTrue(Chars.IsNameChar('9'));
-            Assert.IsTrue(Chars.IsNameChar('.'));
-            Assert.IsTrue(Chars.IsNameChar('_'));
-            Assert.IsFalse(Chars.IsNameChar(':'));
+            isTrue(Chars.IsNameChar('A'));
+            isTrue(Chars.IsNameChar('9'));
+            isFalse(Chars.IsNameChar('.'));
+            isFalse(Chars.IsNameChar('_'));
+            isFalse(Chars.IsNameChar(':'));
+        }
+        [TestMethod] public void IsFullNameCharTest() {
+            isTrue(Chars.IsFullNameChar('A'));
+            isTrue(Chars.IsFullNameChar('9'));
+            isTrue(Chars.IsFullNameChar('.'));
+            isFalse(Chars.IsFullNameChar('_'));
+            isFalse(Chars.IsFullNameChar(':'));
         }
     }
 }
---
+++ Tests/Facade/Party/PrisonerViewFactoryTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MTPrison.Aids;
+using MTPrison.Data.Party;
+using MTPrison.Domain.Party;
+using MTPrison.Facade.Party;
+
+namespace MTPrison.Tests.Facade.Party {
+    [TestClass] public class PrisonerViewFactoryTests : SealedClassTests<PrisonerViewFactory> { 
+        [TestMethod] public void CreateTest() { }
+        [TestMethod] public void CreateViewTest() {
+            var d = GetRandom.Value<PrisonerData>();
+            var e = new Prisoner(d);
+            var v = new PrisonerViewFactory().Create(e);
+            isNotNull(v);
+            //TODO
+            //arePropertiesEaqual(v, e, nameof(v.FullName));
+            areEqual(v.Id, e.Id);
+            areEqual(v.FirstName, e.FirstName);
+            areEqual(v.LastName, e.LastName);
+            areEqual(v.Offense, e.Offense);
+            areEqual(v.DoB, e.DoB);
+            areEqual(v.DateOfImprisonment, e.DateOfImprisonment);
+            areEqual(v.DateOfRelease, e.DateOfRelease);
+            areEqual(v.FullName, e.FullName());
+        }
+        [TestMethod] public void CreateEntityTest() {
+            var v = GetRandom.Value<PrisonerView>();
+            var e = new PrisonerViewFactory().Create(v);
+            isNotNull(e);
+            //TODO
+            //arePropertiesEaqual(e, v);
+            areEqual(e.Id, v.Id);
+            areEqual(e.FirstName, v.FirstName);
+            areEqual(e.LastName, v.LastName);
+            areEqual(e.Offense, v.Offense);
+            areEqual(e.DoB, v.DoB);
+            areEqual(e.DateOfImprisonment, v.DateOfImprisonment);
+            areEqual(e.DateOfRelease, v.DateOfRelease);
+            areNotEqual(e.FullName(), v.FullName);
+        }
+    }
+}
