a0cbe612aa036875febb024bc5fa3f7c7c50866b
markevanluik <markevanluik@gmail.com>
2022-03-16 21:14:20 +0200

    Add a unit test and move test methods to the right class

--- Tests/Domain/Party/PrisonerTests.cs
+++ Tests/Domain/Party/PrisonerTests.cs
@@ -1,10 +1,18 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MTPrison.Data.Party;
 using MTPrison.Domain.Party;
 using System;
 
 namespace MTPrison.Tests.Domain.Party {
     [TestClass] public class IPrisonersRepoTests : IsTypeTested { }
     [TestClass] public class PrisonerTests : SealedClassTests<Prisoner> {
+        private Prisoner? prisoner;
+        [TestInitialize] public void Init() {
+            PrisonerData prisonerData = new PrisonerData();
+            prisonerData.FirstName = "John";
+            prisonerData.LastName = "Doe";
+            prisoner = new Prisoner(prisonerData);
+        }
         [TestMethod] public void IdTest() => isProperty<string?>(null, true);
         [TestMethod] public void FirstNameTest() => isProperty<string?>(null, true);
         [TestMethod] public void LastNameTest() => isProperty<string?>(null, true);
@@ -12,6 +20,6 @@ namespace MTPrison.Tests.Domain.Party {
         [TestMethod] public void DoBTest() => isProperty<DateTime?>(null, true);
         [TestMethod] public void DateOfReleaseTest() => isProperty<DateTime?>(null, true);
         [TestMethod] public void DateOfImprisonmentTest() => isProperty<DateTime?>(null, true);
-        [TestMethod] public void FullNameTest() => isInconclusive();
+        [TestMethod] public void FullNameTest() => areEqual("John Doe", prisoner?.FullName());
     }
 }
