a211b739da627de45f94afa23028b1ccfab88158
taploo22 <tarmoploom@outlook.com>
2022-03-27 00:30:52 +0200

    Added (Cell, Currency)Initializer > Infra

---
+++ Infra/Initializers/CellsInitializer.cs
@@ -0,0 +1,21 @@
+﻿using MTPrison.Data.Party;
+
+namespace MTPrison.Infra.Initializers {
+    public sealed class CellsInitializer : BaseInitializer<CellData> {
+        public CellsInitializer(PrisonDb? db) : base(db, db?.Cells) { }
+        internal static CellData createCell(string id, int cellNumber, int capacity, string type, string section) => new() {
+            Id = id,
+            CellNumber = cellNumber,
+            Capacity = capacity,
+            Type = type,
+            Section = section
+        };
+
+        protected override IEnumerable<CellData> getEntities => new[] {
+            createCell("52345", 1, 3, "Delcuxe", "C"),
+            createCell("62345", 21, 2, "Duo", "B"),
+            createCell("72345", 31, 1, "Solitary", "A"),
+            createCell("82345", 32, 1, "Solitary", "A"),
+        };
+    }
+}
---
+++ Infra/Initializers/CurrenciesInitializer.cs
@@ -0,0 +1,22 @@
+﻿using MTPrison.Data.Party;
+using System.Globalization;
+
+namespace MTPrison.Infra.Initializers {
+    public sealed class CurrenciesInitializer : BaseInitializer<CurrencyData> {
+        public CurrenciesInitializer(PrisonDb? db) : base(db, db?.Currencies) { }
+        protected override IEnumerable<CurrencyData> getEntities {
+            get {
+                var l = new List<CurrencyData>();
+                foreach (CultureInfo cul in CultureInfo.GetCultures(CultureTypes.SpecificCultures)) {
+                    var c = new RegionInfo(new CultureInfo(cul.Name, false).LCID);
+                    var d = createCurrency(c.GeoId, c.ISOCurrencySymbol, c.CurrencyEnglishName, c.CurrencyNativeName);
+                    if (l.FirstOrDefault(x => x.Id == d.Id) is not null) continue;
+                    l.Add(d);
+                }
+                return l;
+            }
+        }
+        internal static CurrencyData createCurrency(int id, string code, string name, string nativeName)
+            => new() { Id = id.ToString(), Code = code, Name = name, NativeName = nativeName };
+    }
+}
