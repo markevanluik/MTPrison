3ac92d0b3b18c8fec552a3d822d3af63a7f55611
taploo22 <tarmoploom@outlook.com>
2022-03-27 00:30:08 +0200

    added (Cell, Currency) Initializing > BaseInitializer

--- Infra/Initializers/BaseInitializer.cs
+++ Infra/Initializers/BaseInitializer.cs
@@ -21,7 +21,10 @@ namespace MTPrison.Infra.Initializers {
     public static class PrisonDbInitializer {
         public static void Init(PrisonDb? db) {
             new PrisonersInitializer(db).Init();
+            new CellsInitializer(db).Init();
             new CountriesInitializer(db).Init();
+            new CurrenciesInitializer(db).Init();
+
         }
     }
 }
