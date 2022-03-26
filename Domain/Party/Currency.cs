5935d9977146e74ebed2655045848e4047d66e66
taploo22 <tarmoploom@outlook.com>
2022-03-27 00:28:41 +0200

    added Currency > Domain

---
+++ Domain/Party/Currency.cs
@@ -0,0 +1,12 @@
+﻿using MTPrison.Data.Party;
+
+namespace MTPrison.Domain.Party {
+    public interface ICurrenciesRepo : IRepo<Currency> { }
+    public sealed class Currency : Entity<CurrencyData>{
+        public Currency() : this(new CurrencyData()) { }
+        public Currency(CurrencyData d) : base(d) { }
+        public string? Code => getValue(Data?.Code);
+        public string? Name => getValue(Data?.Name);
+        public string? NativeName => getValue(Data?.NativeName);
+    }
+}
