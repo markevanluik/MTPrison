b47f574585510469febd51c49a0455710a579153
taploo22 <tarmoploom@outlook.com>
2022-03-27 00:31:23 +0200

    added CurrenciesRepo > Infra/Party

---
+++ Infra/Party/CurrenciesRepo.cs
@@ -0,0 +1,9 @@
+﻿using MTPrison.Data.Party;
+using MTPrison.Domain.Party;
+
+namespace MTPrison.Infra.Party {
+    public class CurrenciesRepo : Repo<Currency, CurrencyData>, ICurrenciesRepo {
+        public CurrenciesRepo(PrisonDb? db) : base(db, db?.Currencies) { }
+        protected override Currency toDomain(CurrencyData d) => new(d);
+    }
+}
