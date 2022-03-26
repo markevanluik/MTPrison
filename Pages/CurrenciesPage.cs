d2217a071465931fafd8c6decec179fe6b6320d9
taploo22 <tarmoploom@outlook.com>
2022-03-27 00:36:16 +0200

    added CurrenciesPage > Pages

---
+++ Pages/CurrenciesPage.cs
@@ -0,0 +1,10 @@
+﻿using MTPrison.Domain.Party;
+using MTPrison.Facade.Party;
+
+namespace MTPrison.Pages {
+    public class CurrenciesPage : BasePage<CurrencyView, Currency, ICurrenciesRepo> {
+        public CurrenciesPage(ICurrenciesRepo r) : base(r) { }
+        protected override Currency toObject(CurrencyView? item) => new CurrencyViewFactory().Create(item);
+        protected override CurrencyView toView(Currency? entity) => new CurrencyViewFactory().Create(entity);
+    }
+}
