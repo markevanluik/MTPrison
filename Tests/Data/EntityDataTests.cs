8673f60c09299740c1ec347cde84fafad220207b
tarmoploom <tarmoploom@outlook.com>
2022-03-30 23:06:01 +0300

    Refactoring Tests > changing reference to > UniqueView

--- Tests/Data/EntityDataTests.cs
+++ Tests/Data/EntityDataTests.cs
@@ -4,7 +4,7 @@ using MTPrison.Facade;
 namespace MTPrison.Tests.Data {
     [TestClass] public class EntityDataTests : AbstractClassTests{
         [TestMethod] public void IdTest() => isProperty<string>();
-        private class testClass : BaseView { }
+        private class testClass : UniqueView { }
         protected override object createObject() => new testClass();
     }
 }
--- Tests/Facade/BaseViewTests.cs
+++ Tests/Facade/BaseViewTests.cs
@@ -4,7 +4,7 @@ using MTPrison.Facade;
 namespace MTPrison.Tests.Facade {
     [TestClass] public class BaseViewTests : AbstractClassTests{
         [TestMethod] public void IdTest() => isProperty<string?>();
-        private class testClass : BaseView { }
+        private class testClass : UniqueView { }
         protected override object createObject() => new testClass();
     }
 }
