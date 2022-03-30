58e9b2165b881032fd267035e8a2959f98d96f36
tarmoploom <tarmoploom@outlook.com>
2022-03-30 23:05:11 +0300

    Refactoring > changed reference to> Unique...

--- Pages/BasePage.cs
+++ Pages/BasePage.cs
@@ -5,8 +5,8 @@ using MTPrison.Facade;
 
 namespace MTPrison.Pages {
     public abstract class BasePage<TView, TEntity, TRepo> : PageModel
-    where TView : BaseView
-    where TEntity : Entity
+    where TView : UniqueView
+    where TEntity : UniqueEntity
     where TRepo : IBaseRepo<TEntity> {
         private readonly TRepo repo;
         protected abstract TView toView(TEntity? entity);
