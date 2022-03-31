23f11f27a39e770d7bb9c261ec66870a26f6d4f2
tarmoploom <tarmoploom@outlook.com>
2022-04-01 00:11:06 +0300

    Refactoring > added keeping track of pages

--- Infra/PagedRepo.cs
+++ Infra/PagedRepo.cs
@@ -4,7 +4,24 @@ using MTPrison.Domain;
 
 namespace MTPrison.Infra {
     public abstract class PagedRepo<TDomain, TData> : OrderedRepo<TDomain, TData>
-    where TDomain : UniqueEntity<TData>, new() where TData : UniqueData, new() {
+        where TDomain : UniqueEntity<TData>, new() where TData : UniqueData, new() {
         protected PagedRepo(DbContext? c, DbSet<TData>? s) : base(c, s) { }
+
+        internal int skippedItemsCount => PageSize * PageIndex;
+        internal static int itemsCountInPage = 10;
+
+        public int PageIndex { get; set; }
+        public int TotalPages => totalPages;
+        public bool HasNextPage => PageIndex < TotalPages - 1;
+        public bool HasPreviousPage => PageIndex > 0;
+        public int PageSize { get; set; } = itemsCountInPage;
+
+        // sql query
+        protected internal override IQueryable<TData> createSql() => addSkipAndTake(base.createSql());
+
+        internal IQueryable<TData> addSkipAndTake(IQueryable<TData> query) => query.Skip(skippedItemsCount).Take(PageSize);
+        internal int totalPages => (int)Math.Ceiling(countPages);
+        internal double countPages => itemsCount / (double)PageSize;
+        internal int itemsCount => base.createSql().Count();
     }
 }
