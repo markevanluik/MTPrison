0cba5c55007d2117d1df4d40fce928e3384334c9
tarmoploom <tarmoploom@outlook.com>
2022-04-01 20:07:57 +0300

    Refactoring > CellsRepo & CurrenciesRepo gets IQueryable

--- Infra/Party/CellsRepo.cs
+++ Infra/Party/CellsRepo.cs
@@ -5,5 +5,15 @@ namespace MTPrison.Infra.Party {
     public class CellsRepo : Repo<Cell, CellData>, ICellsRepo {
         public CellsRepo(PrisonDb? db) : base(db, db?.Cells) { }
         protected override Cell toDomain(CellData d) => new(d);
+
+        internal override IQueryable<CellData> addFilter(IQueryable<CellData> query) {
+            var y = CurrentFilter;
+            if (string.IsNullOrWhiteSpace(y)) return query;
+            return query.Where(
+                x => x.CellNumber.ToString().Contains(y)
+                  || x.Capacity.ToString().Contains(y)
+                  || x.Type.Contains(y)
+                  || x.Section.Contains(y));
+        }
     }
 }
--- Infra/Party/CurrenciesRepo.cs
+++ Infra/Party/CurrenciesRepo.cs
@@ -5,5 +5,14 @@ namespace MTPrison.Infra.Party {
     public class CurrenciesRepo : Repo<Currency, CurrencyData>, ICurrenciesRepo {
         public CurrenciesRepo(PrisonDb? db) : base(db, db?.Currencies) { }
         protected override Currency toDomain(CurrencyData d) => new(d);
+
+        internal override IQueryable<CurrencyData> addFilter(IQueryable<CurrencyData> query) {
+            var y = CurrentFilter;
+            if (string.IsNullOrWhiteSpace(y)) return query;
+            return query.Where(
+                x => x.Code.Contains(y)
+                  || x.Name.Contains(y)
+                  || x.NativeName.Contains(y));
+        }
     }
 }
