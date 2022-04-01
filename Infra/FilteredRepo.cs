e3c75a62669c8ead8eb44e2338de7149e5d31831
tarmoploom <tarmoploom@outlook.com>
2022-04-01 20:09:26 +0300

    Refactoring > FiltereRepo gets CurrentFilter prop. & sql query

--- Infra/FilteredRepo.cs
+++ Infra/FilteredRepo.cs
@@ -6,5 +6,11 @@ namespace MTPrison.Infra {
     public abstract class FilteredRepo<TDomain, TData> : CrudRepo<TDomain, TData>
         where TDomain : UniqueEntity<TData>, new() where TData : UniqueData, new() {
         protected FilteredRepo(DbContext? c, DbSet<TData>? s) : base(c, s) { }
+
+        public string CurrentFilter { get; set; }
+
+        // sql query
+        protected internal override IQueryable<TData> createSql() => addFilter(base.createSql());
+        internal virtual IQueryable<TData> addFilter(IQueryable<TData> query) => query;
     }
 }
