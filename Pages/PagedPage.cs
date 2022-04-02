d9416717b6adbddab25d9cf2b1a7c7ea4bc2c8c2
tarmoploom <tarmoploom@outlook.com>
2022-04-02 05:28:47 +0300

    Refactoring > PagedPage gets SortOrder functionality

--- Pages/PagedPage.cs
+++ Pages/PagedPage.cs
@@ -9,8 +9,12 @@ namespace MTPrison.Pages {
         where TRepo : IPagedRepo<TEntity> {
 
         protected PagedPage(TRepo r) : base(r) { }
+        public string? SortOrder(string propertyName) => repo.SortOrder(propertyName);
 
-        public string? CurrentSort { get; set; }
+        public string? CurrentSort {
+            get => repo.CurrentSort;
+            set => repo.CurrentSort = value;
+        }
         public string? CurrentFilter {
             get => repo.CurrentFilter;
             set => repo.CurrentFilter = value;
@@ -26,11 +30,13 @@ namespace MTPrison.Pages {
         public override IActionResult OnGetCreate(int pageIndex = 0, string? currentFilter = null, string? sortOrder = null) {
             PageIndex = pageIndex;
             CurrentFilter = currentFilter;
+            CurrentSort = sortOrder;
             return base.OnGetCreate(pageIndex, currentFilter, sortOrder);
         }
         public override async Task<IActionResult> OnPostCreateAsync(int pageIndex = 0, string? currentFilter = null, string? sortOrder = null) {
             PageIndex = pageIndex;
             CurrentFilter = currentFilter;
+            CurrentSort = sortOrder;
             ModelState.Remove(nameof(currentFilter));
             ModelState.Remove(nameof(sortOrder));
             return await base.OnPostCreateAsync(pageIndex, currentFilter, sortOrder);
@@ -38,11 +44,13 @@ namespace MTPrison.Pages {
         public override async Task<IActionResult> OnGetEditAsync(string id, int pageIndex = 0, string? currentFilter = null, string? sortOrder = null) {
             PageIndex = pageIndex;
             CurrentFilter = currentFilter;
+            CurrentSort = sortOrder;
             return await base.OnGetEditAsync(id, pageIndex, currentFilter, sortOrder);
         }
         public override async Task<IActionResult> OnPostEditAsync(int pageIndex = 0, string? currentFilter = null, string? sortOrder = null) {
             PageIndex = pageIndex;
             CurrentFilter = currentFilter;
+            CurrentSort = sortOrder;
             ModelState.Remove(nameof(currentFilter));
             ModelState.Remove(nameof(sortOrder));
             return await base.OnPostEditAsync(pageIndex, currentFilter, sortOrder);
@@ -50,21 +58,25 @@ namespace MTPrison.Pages {
         public override async Task<IActionResult> OnGetDetailsAsync(string id, int pageIndex = 0, string? currentFilter = null, string? sortOrder = null) {
             PageIndex = pageIndex;
             CurrentFilter = currentFilter;
+            CurrentSort = sortOrder;
             return await base.OnGetDetailsAsync(id, pageIndex, currentFilter, sortOrder);
         }
         public override async Task<IActionResult> OnGetDeleteAsync(string id, int pageIndex = 0, string? currentFilter = null, string? sortOrder = null) {
             PageIndex = pageIndex;
             CurrentFilter = currentFilter;
+            CurrentSort = sortOrder;
             return await base.OnGetDeleteAsync(id, pageIndex, currentFilter, sortOrder);
         }
         public override async Task<IActionResult> OnPostDeleteAsync(string id, int pageIndex = 0, string? currentFilter = null, string? sortOrder = null) {
             PageIndex = pageIndex;
             CurrentFilter = currentFilter;
+            CurrentSort = sortOrder;
             return await base.OnPostDeleteAsync(id, pageIndex, currentFilter, sortOrder);
         }
         public override async Task<IActionResult> OnGetIndexAsync(int pageIndex = 0, string? currentFilter = null, string? sortOrder = null) {
             PageIndex = pageIndex;
             CurrentFilter = currentFilter;
+            CurrentSort = sortOrder;
             return await base.OnGetIndexAsync(pageIndex, currentFilter, sortOrder);
         }
     }
