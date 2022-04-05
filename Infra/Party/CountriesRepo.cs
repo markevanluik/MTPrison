d99ffd2339fc1226fbadab34da8332ded581a7bd
markevanluik <markevanluik@gmail.com>
2022-04-05 17:42:32 +0300

    Add addFilter method to CountriesRepo and PrisonersRepo

--- Infra/Party/CountriesRepo.cs
+++ Infra/Party/CountriesRepo.cs
@@ -5,5 +5,13 @@ namespace MTPrison.Infra.Party {
     public class CountriesRepo : Repo<Country, CountryData>, ICountriesRepo {
         public CountriesRepo(PrisonDb? db) : base(db, db?.Countries) { }
         protected override Country toDomain(CountryData d) => new(d);
+        internal override IQueryable<CountryData> addFilter(IQueryable<CountryData> query) {
+            var y = CurrentFilter;
+            if (string.IsNullOrWhiteSpace(y)) return query;
+            return query.Where(
+                x => x.Code.Contains(y)
+                  || x.Name.Contains(y)
+                  || x.NativeName.Contains(y));
+        }
     }
 }
--- Infra/Party/PrisonersRepo.cs
+++ Infra/Party/PrisonersRepo.cs
@@ -5,5 +5,13 @@ namespace MTPrison.Infra.Party {
     public class PrisonersRepo : Repo<Prisoner, PrisonerData>, IPrisonersRepo {
         public PrisonersRepo(PrisonDb? db) : base(db, db?.Prisoners) { }
         protected override Prisoner toDomain(PrisonerData d) => new(d);
+        internal override IQueryable<PrisonerData> addFilter(IQueryable<PrisonerData> query) {
+            var y = CurrentFilter;
+            if (string.IsNullOrWhiteSpace(y)) return query;
+            return query.Where(
+                x => x.FirstName.ToString().Contains(y)
+                  || x.LastName.ToString().Contains(y)
+                  || x.Offense.Contains(y));
+        }
     }
 }
