c3e9489be4c1ec22e780f47d6af350d8cf5c2856
markevanluik <markevanluik@gmail.com>
2022-04-05 17:40:55 +0300

    Add IsoCommonView to CountryView and CurrencyView

--- Facade/Party/CountryView.cs
+++ Facade/Party/CountryView.cs
@@ -1,13 +1,8 @@
 ﻿using MTPrison.Data.Party;
 using MTPrison.Domain.Party;
-using System.ComponentModel.DataAnnotations;
 
 namespace MTPrison.Facade.Party {
-    public class CountryView : UniqueView {
-        [Display(Name = "Code")][MaxLength(3)][Required] public string Code { get; set; }
-        [Display(Name = "Name")][Required] public string? Name { get; set; }
-        [Display(Name = "Native Name")][Required] public string? NativeName { get; set; }
-    }
+    public class CountryView : IsoCommonView { }
     public sealed class CountryViewFactory : BaseViewFactory<CountryView, Country, CountryData> {
         protected override Country toEntity(CountryData d) => new(d);
 
--- Facade/Party/CurrencyView.cs
+++ Facade/Party/CurrencyView.cs
@@ -2,7 +2,7 @@
 using MTPrison.Domain.Party;
 
 namespace MTPrison.Facade.Party {
-    public class CurrencyView : CommonView { }
+    public class CurrencyView : IsoCommonView { }
     public sealed class CurrencyViewFactory : BaseViewFactory<CurrencyView, Currency, CurrencyData> {
         protected override Currency toEntity(CurrencyData d) => new(d);
     }
