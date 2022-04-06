e73b239ecc23b9e4af29b4eb3b3558b53f5c7eb7
tarmoploom <tarmoploom@outlook.com>
2022-04-06 22:23:03 +0300

    Refactoring > added IndexColumns

--- Pages/Party/CellsPage.cs
+++ Pages/Party/CellsPage.cs
@@ -6,5 +6,11 @@ namespace MTPrison.Pages.Party {
         public CellsPage(ICellsRepo r) : base(r) { }
         protected override Cell toObject(CellView? item) => new CellViewFactory().Create(item);
         protected override CellView toView(Cell? entity) => new CellViewFactory().Create(entity);
+        public override string[] IndexColumns { get; } = new[] {
+            nameof(CellView.CellNumber),
+            nameof(CellView.Capacity),
+            nameof(CellView.Type),
+            nameof(CellView.Section)
+        };
     }
 }
--- Pages/Party/CurrenciesPage.cs
+++ Pages/Party/CurrenciesPage.cs
@@ -6,5 +6,10 @@ namespace MTPrison.Pages.Party {
         public CurrenciesPage(ICurrenciesRepo r) : base(r) { }
         protected override Currency toObject(CurrencyView? item) => new CurrencyViewFactory().Create(item);
         protected override CurrencyView toView(Currency? entity) => new CurrencyViewFactory().Create(entity);
+        public override string[] IndexColumns { get; } = new[] {
+            nameof(CurrencyView.Code),
+            nameof(CurrencyView.Name),
+            nameof(CurrencyView.NativeName)
+        };
     }
 }
