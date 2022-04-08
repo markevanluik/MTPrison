25a3903f4d81806c70afcd981e2ae5ad46cb58da
tarmoploom <tarmoploom@outlook.com>
2022-04-08 04:22:51 +0300

    Refactoring, CellData get new properties

--- Data/Party/CellData.cs
+++ Data/Party/CellData.cs
@@ -5,6 +5,10 @@ namespace MTPrison.Data.Party {
         public int Capacity { get; set; }
         public string? Type { get; set; }
         public string? Section { get; set; }
+        public string? Country { get; set; }
+        public DateTime? Inspection { get; set; }
+        public IsoGender? Gender { get; set; }
+
 
         //public List<Prisoner> Occupants { get; set; }
     }
