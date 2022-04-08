6c80c125ca0172ab1d1e7e762f270616caaaeb0a
tarmoploom <tarmoploom@outlook.com>
2022-04-08 04:24:54 +0300

    Refactoring > UniqueEntity gets IsoGender default

--- Domain/UniqueEntity.cs
+++ Domain/UniqueEntity.cs
@@ -1,4 +1,5 @@
 ﻿using MTPrison.Data;
+using MTPrison.Data.Party;
 
 namespace MTPrison.Domain {
     public abstract class UniqueEntity {
@@ -8,6 +9,7 @@ namespace MTPrison.Domain {
         protected static string getValue(string? v) => v ?? DefaultStr;
         protected static DateTime getValue(DateTime? v) => v ?? defaultDate;
         protected static int getValue(int? v) => v ?? defaultInt;
+        protected static IsoGender getValue(IsoGender? v) => v ?? IsoGender.NotApplicable;
     }
     public abstract class UniqueEntity<TData> : UniqueEntity where TData : UniqueData, new() {
         private readonly TData data;
