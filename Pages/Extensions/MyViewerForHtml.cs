b2defd0ce49444448ac7e772a79f9d4f70aae186
tarmoploom <tarmoploom@outlook.com>
2022-04-08 04:33:50 +0300

    Refactoring > MyViewerHtml gets second method with extra dynamic value

--- Pages/Extensions/MyViewerForHtml.cs
+++ Pages/Extensions/MyViewerForHtml.cs
@@ -9,6 +9,11 @@ namespace MTPrison.Pages.Extensions {
             var s = htmlStrings(h, e);
             return new HtmlContentBuilder(s);
         }
+        public static IHtmlContent MyViewerFor<TModel, TResult>(
+            this IHtmlHelper<TModel> html, Expression<Func<TModel, TResult>> e, dynamic value) {
+            var s = htmlStrings(html, e, value);
+            return new HtmlContentBuilder(s);
+        }
 
         private static List<object> htmlStrings<TModel, TResult>(IHtmlHelper<TModel> h, Expression<Func<TModel, TResult>> e) {
             var l = new List<object>();
@@ -20,6 +25,18 @@ namespace MTPrison.Pages.Extensions {
             l.Add(h.DisplayFor(e));
             l.Add(new HtmlString("</dd>"));
             l.Add(new HtmlString("</dl>"));
+            return l;
+        }
+        private static List<object> htmlStrings<TModel, TResult>(IHtmlHelper<TModel> html, Expression<Func<TModel, TResult>> e, dynamic value) {
+            var l = new List<object> {
+                new HtmlString("<dl class=\"row\">"),
+                new HtmlString("<dt class=\"col-sm-2\">"),
+                html.DisplayNameFor(e),
+                new HtmlString("</dt>"),
+                new HtmlString("<dd class=\"col-sm-10\">"),
+                html.Raw(value),
+                new HtmlString("</dd>"),
+                new HtmlString("</dl>")};
             return l;
         }
     }
