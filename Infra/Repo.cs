3681ab88c2897645d4788fd6dc0d514522006c1e
tarmoploom <tarmoploom@outlook.com>
2022-04-08 04:30:48 +0300

    Refactoring > Repo gets contains(...) mehod

--- Infra/Repo.cs
+++ Infra/Repo.cs
@@ -1,10 +1,11 @@
-﻿using MTPrison.Domain;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using MTPrison.Domain;
 using MTPrison.Data;
 
 namespace MTPrison.Infra {
     public abstract class Repo<TDomain, TData> : PagedRepo<TDomain, TData>
         where TDomain : UniqueEntity<TData>, new() where TData : UniqueData, new() {
         protected Repo(DbContext? c, DbSet<TData>? s) : base(c, s) { }
+        internal protected static bool contains(string? value, string s) => (s is null) || (value?.Contains(s) ?? false);
     }
 }
