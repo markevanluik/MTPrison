93b5aa74b0c68d3e5beac73cfb783839fb799a59
tarmoploom <tarmoploom@outlook.com>
2022-04-09 22:51:59 +0300

    Refactoring > Countries/CurrenciesPage gets opposite Lists

--- Pages/Party/CountriesPage.cs
+++ Pages/Party/CountriesPage.cs
@@ -6,5 +6,6 @@ namespace MTPrison.Pages.Party {
         public CountriesPage(ICountriesRepo r) : base(r) { }
         protected override Country toObject(CountryView? item) => new CountryViewFactory().Create(item);
         protected override CountryView toView(Country? entity) => new CountryViewFactory().Create(entity);
+        public List<Currency?> Currencies => toObject(Item).Currencies;
     }
 }
