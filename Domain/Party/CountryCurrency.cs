3ee5846ccf3bd0da0710ace9585d9271fd9d8dd0
tarmoploom <tarmoploom@outlook.com>
2022-04-09 21:23:25 +0300

    Added 2 new Domain classes > PrisCe.. & CountCr..

---
+++ Domain/Party/CountryCurrency.cs
@@ -0,0 +1,17 @@
+﻿using MTPrison.Data.Party;
+
+namespace MTPrison.Domain.Party {
+    public interface ICountryCurrenciesRepo : IRepo<CountryCurrency> { }
+    public class CountryCurrency : NamedEntity<CountryCurrencyData> {
+        // constructors
+        public CountryCurrency() : this(new()) { }
+        public CountryCurrency(CountryCurrencyData d) : base(d) { }
+        //
+        public string CountryId => getValue(Data?.CountryId);
+        public string CurrencyId => getValue(Data?.CurrencyId);
+
+        //Later
+        //public Country? Country => GetRepo.Instance<ICountriesRepo>()?.Get(CountryId);
+        //public Currency? Currency => GetRepo.Instance<ICurrenciesRepo>()?.Get(CurrencyId);
+    }
+}
