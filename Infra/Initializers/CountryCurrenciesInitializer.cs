f6a9f588dd5b649f926f0efd30b6639f48393f25
tarmoploom <tarmoploom@outlook.com>
2022-04-17 19:15:58 +0300

    Refactoring > CountryCurrencyInitializer List adds only uniqe-instances

--- Infra/Initializers/CountryCurrenciesInitializer.cs
+++ Infra/Initializers/CountryCurrenciesInitializer.cs
@@ -5,17 +5,16 @@ using System.Globalization;
 namespace MTPrison.Infra.Initializers {
     public sealed class CountryCurrenciesInitializer : BaseInitializer<CountryCurrencyData> {
         public CountryCurrenciesInitializer(PrisonDb? db) : base(db, db?.CountryCurrencies) { }
-
+        private readonly List<CountryCurrencyData> l = new();
         protected override IEnumerable<CountryCurrencyData> getEntities {
             get {
-                var l = new List<CountryCurrencyData>();
                 foreach (CultureInfo cul in CultureInfo.GetCultures(CultureTypes.SpecificCultures)) {
                     var c = new RegionInfo(new CultureInfo(cul.Name, false).LCID);
                     var coId = c.ThreeLetterISORegionName;
                     if (!isCorrectIsoCode(coId)) continue;
                     if (l.FirstOrDefault(x => x.Id == coId) is not null) continue;
                     var data = createCountry(coId, c.ISOCurrencySymbol, c.CurrencySymbol, c.EnglishName, c.CurrencyEnglishName, c.CurrencyNativeName);
-                    l.Add(data);
+                    addUniqueDataToList(data);
                 }
                 return l;
             }
@@ -30,5 +29,18 @@ namespace MTPrison.Infra.Initializers {
             CurrencyName = cuName,
             NativeName = nativeName
         };
+        private void addUniqueDataToList(CountryCurrencyData d) {
+            bool locked = false;
+            foreach (var i in l) {
+                if (i.CountryId == d.CountryId &&
+                    i.CurrencyId == d.CurrencyId &&
+                    i.Code == d.Code &&
+                    i.Name == d.Name &&
+                    i.CountryName == d.CountryName &&
+                    i.CurrencyName == d.CurrencyName &&
+                    i.NativeName == d.NativeName) locked = true;
+            }
+            if (!locked) l.Add(d);
+        }
     }
 }
