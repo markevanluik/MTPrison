d38f2bba705ce465499f11b07e4c303032035c92
tarmoploom <tarmoploom@outlook.com>
2022-04-23 20:28:08 +0300

    Refactoring > minor changes to Chars & GetAssembly

--- Aids/Chars.cs
+++ Aids/Chars.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace MTPrison.Aids {
     public static class Chars {
-        public static bool IsNameChar(this char x) => char.IsLetter(x);
-        public static bool IsFullNameChar(this char x) => IsNameChar(x) || x == '.' || x == '`';
+        public static bool IsNameChar(this char c) => char.IsLetterOrDigit(c) || c == '`';
+        public static bool IsFullNameChar(this char c) => IsNameChar(c) || c == '.';
     }
 }
