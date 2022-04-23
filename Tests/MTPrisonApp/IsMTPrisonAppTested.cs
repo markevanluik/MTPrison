ae8a838946dacd384754806bc9ccc5542e239c34
tarmoploom <tarmoploom@outlook.com>
2022-04-23 20:47:42 +0300

    Refactoring > small change

--- Tests/MTPrisonApp/IsMTPrisonAppTested.cs
+++ Tests/MTPrisonApp/IsMTPrisonAppTested.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace MTPrison.Tests.MTPrisonApp {
-    [TestClass] public class IsMTPrisonAppTested : IsAssemblyTested {
+    [TestClass] public class IsMTPrisonAppTested : AssemblyTests {
         protected override void isAllTested() => isInconclusive("Namespace has to be changed to \"MTPrison.MTPisonApp\"");
     }
 }
