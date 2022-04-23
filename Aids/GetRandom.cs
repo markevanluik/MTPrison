b771f1c0a48cd0d12e0d859ac57e9ea44767521a
tarmoploom <tarmoploom@outlook.com>
2022-04-23 20:30:00 +0300

    Refactoring > GetRandom gets revised Value methods & new EnumOf/getUnderlyingType

--- Aids/GetRandom.cs
+++ Aids/GetRandom.cs
@@ -24,6 +24,7 @@ namespace MTPrison.Aids {
             minFirst(ref minVal, ref maxVal);
             return minVal + Random.Shared.NextDouble() * (maxVal - minVal);
         }
+
         public static char Char(char min = char.MinValue, char max = char.MaxValue) => (char)Int32(min, max);
         public static bool Bool() => Int32() % 2 == 0;
         public static DateTime DateTime(DateTime? min = null, DateTime? max = null) {
@@ -40,38 +41,29 @@ namespace MTPrison.Aids {
             return s;
         }
         public static dynamic? Value<T>(T? min = default, T? max = default) {
-            if (typeof(T) == typeof(bool)) return Bool();
-            else if (typeof(T) == typeof(bool?)) return Bool();
-            else if (typeof(T) == typeof(DateTime)) return DateTime(Convert.ToDateTime(min), Convert.ToDateTime(max));
-            else if (typeof(T) == typeof(DateTime?)) return DateTime(Convert.ToDateTime(min), Convert.ToDateTime(max));
-            else if (typeof(T) == typeof(double?)) return Double(Convert.ToDouble(min), Convert.ToDouble(max));
-            else if (typeof(T) == typeof(double)) return Double(Convert.ToDouble(min), Convert.ToDouble(max));
-            else if (typeof(T) == typeof(int?)) return Int32(Convert.ToInt32(min), Convert.ToInt32(max));
-            else if (typeof(T) == typeof(int)) return Int32(Convert.ToInt32(min), Convert.ToInt32(max));
-            else if (typeof(T) == typeof(long?)) return Int64(Convert.ToInt64(min), Convert.ToInt64(max));
-            else if (typeof(T) == typeof(long)) return Int64(Convert.ToInt64(min), Convert.ToInt64(max));
-            else if (typeof(T) == typeof(char?)) return Char(Convert.ToChar(min), Convert.ToChar(max));
-            else if (typeof(T) == typeof(char)) return Char(Convert.ToChar(min), Convert.ToChar(max));
-            else if (typeof(T) == typeof(string)) return String();
+            var t = getUnderlyingType(typeof(T));
+            if (isEnum(t)) return EnumOf<T>();
+            else if (t == typeof(bool)) return Bool();
+            else if (t == typeof(int)) return Int32(Convert.ToInt32(min), Convert.ToInt32(max));
+            else if (t == typeof(long)) return Int64(Convert.ToInt64(min), Convert.ToInt64(max));
+            else if (t == typeof(double)) return Double(Convert.ToDouble(min), Convert.ToDouble(max));
+            else if (t == typeof(char)) return Char(Convert.ToChar(min), Convert.ToChar(max));
+            else if (t == typeof(DateTime)) return DateTime(Convert.ToDateTime(min), Convert.ToDateTime(max));
+            else if (t == typeof(string)) return String();
             return tryGetObject<T>();
         }
         public static dynamic? Value(Type t) {
-            if (t == typeof(bool)) return Bool();
-            else if (t == typeof(bool?)) return Bool();
-            else if (t == typeof(DateTime)) return DateTime();
-            else if (t == typeof(DateTime?)) return DateTime();
-            else if (t == typeof(double?)) return Double();
-            else if (t == typeof(double)) return Double();
-            else if (t == typeof(int?)) return Int32();
+            t = getUnderlyingType(t);
+            if (isEnum(t)) return EnumOf(t);
+            else if (t == typeof(bool)) return Bool();
             else if (t == typeof(int)) return Int32();
-            else if (t == typeof(long?)) return Int64();
             else if (t == typeof(long)) return Int64();
-            else if (t == typeof(char?)) return Char();
+            else if (t == typeof(double)) return Double();
             else if (t == typeof(char)) return Char();
+            else if (t == typeof(DateTime)) return DateTime();
             else if (t == typeof(string)) return String();
             return null;
         }
-
         private static T? tryGetObject<T>() {
             var o = tryCreate<T>();
             foreach (var pi in o?.GetType()?.GetProperties() ?? Array.Empty<PropertyInfo>()) {
@@ -83,6 +75,20 @@ namespace MTPrison.Aids {
         }
         private static T? tryCreate<T>() => Safe.Run(() => {
             var c = typeof(T).GetConstructor(Array.Empty<Type>());
-            return (c?.Invoke(null) is T t) ? t : default; });
+            return (c?.Invoke(null) is T t) ? t : default;
+        });
+        public static dynamic? EnumOf<T>() => EnumOf(typeof(T));
+        public static dynamic? EnumOf(Type t) {
+            if (!t.IsEnum) return null;
+            var values = Enum.GetValues(t);
+            var max = values.Length - 1;
+            var i = Int32(0, max);
+            return values.GetValue(i);
+        }
+        internal static bool isEnum(Type t) => t.IsEnum;
+        internal static Type getUnderlyingType(Type t) {
+            var x = Nullable.GetUnderlyingType(t);
+            return x is null ? t : x;
+        }
     }
 }
