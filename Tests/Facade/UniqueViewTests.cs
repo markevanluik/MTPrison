c4c1077f5704c231b43adb06e8c9d7f4863b3d5a
tarmoploom <tarmoploom@outlook.com>
2022-04-23 20:46:09 +0300

    Added UniqueViewTests > Facade

---
+++ Tests/Facade/UniqueViewTests.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MTPrison.Facade;
+
+namespace MTPrison.Tests.Facade {
+    [TestClass] public class UniqueViewTests : AbstractClassTests<UniqueView, object> {
+        private class testClass : UniqueView { }
+        protected override UniqueView createObj() => new testClass();
+        [TestMethod] public void IdTest() => isProperty<string>();
+    }
+}
