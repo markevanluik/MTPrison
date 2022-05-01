7398f61f21c0fe88ba08ee08faba42fb3701703a
tarmoploom <tarmoploom@outlook.com>
2022-05-01 19:02:06 +0300

    Refactoring > Countries small fix + _Layout smaller space between 2 / 2 classes

--- Pages/Party/CountriesPage.cs
+++ Pages/Party/CountriesPage.cs
@@ -6,6 +6,11 @@ namespace MTPrison.Pages.Party {
         public CountriesPage(ICountriesRepo r) : base(r) { }
         protected override Country toObject(CountryView? item) => new CountryViewFactory().Create(item);
         protected override CountryView toView(Country? entity) => new CountryViewFactory().Create(entity);
+        public override string[] IndexColumns { get; } = new[] {
+            nameof(CountryView.Code),
+            nameof(CountryView.Name),
+            nameof(CountryView.NativeName)
+        };
         public List<Currency?> Currencies => toObject(Item).Currencies;
     }
 }
