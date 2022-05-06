48ee9f2a2bfb1fab3eb7e5fe688f140820af8a0a
tarmoploom <tarmoploom@outlook.com>
2022-05-07 02:44:06 +0300

    Refactoring > IRepo GetAll parameter > dynamic

--- Domain/IRepo.cs
+++ Domain/IRepo.cs
@@ -4,7 +4,7 @@ namespace MTPrison.Domain {
         //CRUD
         bool Add(T obj);
         List<T> Get();
-        List<T> GetAll<TKey>(Func<T, TKey>? orderBy = null);
+        List<T> GetAll(Func<T, dynamic>? orderBy = null);
         T Get(string id);
         bool Update(T obj);
         bool Delete(string id);
