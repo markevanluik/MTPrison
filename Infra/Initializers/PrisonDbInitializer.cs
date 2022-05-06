49008db35ee9ffc35e596b154ce5d9766ec5e073
tarmoploom <tarmoploom@outlook.com>
2022-05-07 02:46:05 +0300

    ...

---
+++ Infra/Initializers/PrisonDbInitializer.cs
@@ -0,0 +1,12 @@
+﻿namespace MTPrison.Infra.Initializers {
+    public static class PrisonDbInitializer {
+        public static void Init(PrisonDb? db) {
+            new PrisonersInitializer(db).Init();
+            new CellsInitializer(db).Init();
+            new CountriesInitializer(db).Init();
+            new CurrenciesInitializer(db).Init();
+            new PrisonerCellsInitializer(db).Init();
+            new CountryCurrenciesInitializer(db).Init();
+        }
+    }
+}
