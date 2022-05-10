60e233f22af39d590cb5de1d820a2dc6020b71f9
tarmoploom <tarmoploom@outlook.com>
2022-05-10 18:23:05 +0300

    Added 2 new classes to Core > ConcurrencyToken + abstract copy

---
+++ Core/ConcurrencyToken.cs
@@ -0,0 +1,10 @@
+﻿
+namespace MTPrison.Core {
+    public static class ConcurrencyToken {
+        public static string ToStr(byte[]? token = null) {
+            var s = string.Empty;
+            foreach (var b in token ?? Array.Empty<byte>()) s += b;
+            return s;
+        }
+    }
+}
