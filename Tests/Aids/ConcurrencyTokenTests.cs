d7d11240dfabf6745bbcc3c000608fa6301835f7
tarmoploom <tarmoploom@outlook.com>
2022-05-12 16:57:45 +0300

    Added ConcurrencyTokenTest & CopyTest

---
+++ Tests/Aids/ConcurrencyTokenTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MTPrison.Aids;
+
+namespace MTPrison.Tests.Aids {
+    [TestClass] public class ConcurrencyTokenTests : TypeTests {
+        [TestMethod] public void ToStrTest() {
+            byte[] b = { 0, 1, 124, 255 };
+            areEqual("01124255", ConcurrencyToken.ToStr(b));
+            areEqual("", ConcurrencyToken.ToStr(null));
+        }
+
+        [TestMethod] public void ToByteArrayTest() {
+            areEqual(8, ConcurrencyToken.ToByteArray(null).Length);
+            areEqual(10, ConcurrencyToken.ToByteArray(GetRandom.String(10, 10)).Length);
+        }
+    }
+}
---
+++ Tests/Aids/CopyTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MTPrison.Aids;
+using MTPrison.Data.Party;
+using MTPrison.Facade.Party;
+
+namespace MTPrison.Tests.Aids {
+    [TestClass] public class CopyTests : TypeTests {
+        [TestMethod] public void PropertiesTest() {
+            var d = GetRandom.Value<CountryData>();
+            var v = new CountryView();
+            Copy.Properties(d, v);
+            arePropertiesEqual(d, v);
+
+            v = GetRandom.Value<CountryView>();
+            d = new CountryData();
+            Copy.Properties(v, d);
+            arePropertiesEqual(v, d);
+        }
+    }
+}
