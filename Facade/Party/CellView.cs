5c906c8ac5565a9f525df016af64e8764b9615f2
tarmoploom <tarmoploom@outlook.com>
2022-05-13 22:38:11 +0300

    Added > unique CellNumber check for CellView + Testing

--- Facade/Party/CellView.cs
+++ Facade/Party/CellView.cs
@@ -1,14 +1,26 @@
 ﻿using MTPrison.Data.Party;
+using MTPrison.Domain;
 using MTPrison.Domain.Party;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace MTPrison.Facade.Party {
     public sealed class CellView : UniqueView {
-        [Required, DisplayName("Cell Number"), Range(1, 10000)] public int CellNumber { get; set; }
+
+        [Required, DisplayName("Cell Number"), Range(1, 1000),  Compare(nameof(CheckCellNr), ErrorMessage =$"Cell number already in use.")]
+        public int CellNumber { get; set; }
         [Required, DisplayName("Section")] public string? Section { get; set; }
         [DisplayName("Capacity"), Range(1, 1000)] public int Capacity { get; set; }
         [Required, DisplayName("Type")] public CellType? Type { get; set; }
+        public int CheckCellNr {
+            get {
+                var l = GetRepo.Instance<ICellsRepo>()?
+                    .GetAll(x => x.Id)?
+                    .Where(x => x.CellNumber == CellNumber)?
+                    .FirstOrDefault();
+                return l?.CellNumber == CellNumber ? -1 : CellNumber;
+            }
+        }
     }
     public sealed class CellViewFactory : BaseViewFactory<CellView, Cell, CellData> {
         protected override Cell toEntity(CellData d) => new(d);
--- Tests/Facade/Party/CellViewTests.cs
+++ Tests/Facade/Party/CellViewTests.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MTPrison.Aids;
 using MTPrison.Data.Party;
+using MTPrison.Domain;
+using MTPrison.Domain.Party;
 using MTPrison.Facade;
 using MTPrison.Facade.Party;
 
@@ -8,6 +11,21 @@ namespace MTPrison.Tests.Facade.Party {
         [TestMethod] public void CellNumberTest() => isRequired<int>("Cell Number");
         [TestMethod] public void CapacityTest() => isDisplayNamed<int>("Capacity");
         [TestMethod] public void TypeTest() => isRequired<CellType>("Type");
-        [TestMethod] public void SectionTest() => isProperty<string>("Section");
+        [TestMethod] public void SectionTest() => isProperty("Section");
+        [TestMethod] public void CheckCellNrTest() {
+            CellData ? d = GetRandom.Value<CellData>();
+            isNotNull(d);
+            d.CellNumber = 1;
+            var e = new Cell(d);
+            var v = new CellViewFactory().Create(e);
+            areEqual(v.CheckCellNr, e.CellNumber);
+
+            var l = GetRepo.Instance<ICellsRepo>();
+            isNotNull(l);
+            l?.Add(e);
+            v = new CellViewFactory().Create(e);
+            areNotEqual(v.CheckCellNr, e.CellNumber);
+            areEqual(v.CheckCellNr, -1);
+        }
     }
 }
